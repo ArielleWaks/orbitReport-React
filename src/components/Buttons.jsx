128ec0f4052aa7f066ef780e7d0015955d2be8e0
ArielleWaks <ariellemwaks@gmail.com>
2023-11-14 15:37:29 -0500

    created first two buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      
+      <button onClick={() => setSat(satData)} >All Orbits</button>
       <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <button>Placeholder 2</button>
     </div>
   );
 };
