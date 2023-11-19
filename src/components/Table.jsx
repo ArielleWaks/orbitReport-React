94a0564bc6ca35b3c497edfd2baba489b323d17e
ArielleWaks <ariellemwaks@gmail.com>
2023-11-19 17:28:37 -0600

    deleted commented-out code

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -26,28 +26,3 @@ const Table = ({ sat }) => {
 };
 
 export default Table;
-
-// const Table = ({ sat }) => {
-//   return (
-//     <table>
-//       <thead>
-//         <tr>
-//           <th>Name</th>
-//           <th>Type of Satellite</th>
-//           <th>Launch Date</th>
-//           <th>Status</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         <tr key={id} >
-//           <td>{data.name}</td>
-//           <td>{data.type}</td>
-//           <td>{data.launchDate}</td>
-//           <td>{data.operational}</td>
-//         </tr>
-//       </tbody>
-//     </table>
-//   );
-// };
-
-// export default Table;
